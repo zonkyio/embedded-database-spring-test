4f5e6cee5a36e99a56cbe63765a16575826dae94
Tomáš Vaněk <tomix26@gmail.com>
2021-06-13 19:18:28 +0200

    #163 fix database replace mode when configured via application properties

---
+++ embedded-database-spring-test/src/test/java/io/zonky/test/db/SpringBootCustomDatabaseIntegrationTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.zonky.test.db;
+
+import com.google.common.collect.ImmutableMap;
+import io.zonky.test.category.FlywayTestSuite;
+import io.zonky.test.support.ConditionalTestRule;
+import io.zonky.test.support.TestAssumptions;
+import org.flywaydb.test.annotation.FlywayTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+
+import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+@RunWith(SpringRunner.class)
+@Category(FlywayTestSuite.class)
+@AutoConfigureEmbeddedDatabase(type = POSTGRES)
+@TestPropertySource(properties = {
+        "zonky.test.database.replace=none", // the most important part of this test
+
+        "liquibase.enabled=false",
+        "spring.liquibase.enabled=false",
+
+        "flyway.schemas=test",
+        "spring.flyway.schemas=test"
+})
+@JdbcTest
+@ContextConfiguration(initializers = SpringBootCustomDatabaseIntegrationTest.DockerPropertiesInitializer.class)
+public class SpringBootCustomDatabaseIntegrationTest {
+
+    private static final String SQL_SELECT_PERSONS = "select * from test.person";
+
+    @ClassRule
+    public static ConditionalTestRule conditionalTestRule = new ConditionalTestRule(TestAssumptions::assumeSpringBootSupportsJdbcTestAnnotation);
+
+    @ClassRule
+    public static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:11-alpine");
+
+    public static class DockerPropertiesInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            applicationContext.getEnvironment().getPropertySources().addFirst(new MapPropertySource(
+                    SpringBootCustomDatabaseIntegrationTest.class.getSimpleName(), ImmutableMap.of(
+                    "spring.datasource.url", postgresContainer.getJdbcUrl(),
+                    "spring.datasource.username", postgresContainer.getUsername(),
+                    "spring.datasource.password", postgresContainer.getPassword()
+            )));
+        }
+    }
+
+    @Configuration
+    static class Config {
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+    }
+
+    // omitted
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    @FlywayTest
+    public void testJdbcTemplate() {
+        assertThat(jdbcTemplate).isNotNull();
+
+        List<Map<String, Object>> persons = jdbcTemplate.queryForList(SQL_SELECT_PERSONS);
+        assertThat(persons).isNotNull().hasSize(1);
+
+        Map<String, Object> person = persons.get(0);
+        assertThat(person).containsExactly(
+                entry("id", 1L),
+                entry("first_name", "Dave"),
+                entry("last_name", "Syer"));
+    }
+}
